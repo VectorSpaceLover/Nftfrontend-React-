85b9387ddb8dccde507cab046269f3c934f73822
venus <venus9023gold@mail.ru>
2021-11-09 22:28:09 -0800

    react frontend for nft site

---
+++ src/favicons/banner.js
@@ -0,0 +1,56 @@
+let bannerdata = {    
+    "ads": [
+        "images/top/1.png",
+        "images/top/2.png",
+        "images/top/2.png",
+        "images/top/S.png",
+        "images/top/q.png",
+        "images/top/g.png",
+        "images/top/h.png",
+        "images/top/y.png",
+        "images/top/u.png",
+        "images/top/o.png",
+        "images/top/aa.png",
+        "images/top/ju.png",
+        "images/top/ui.png"
+    ],
+    "ads_selected": [1,3,5,6,7,8,11],   // top banner 1st image,   3,5,6,7,8,11  will be added in table
+    "tweets":[
+        "images/top/ta.png",
+        "images/top/tweets11.png",
+        "images/top/t1.png",
+        "images/top/t6.png",
+        "images/top/t9.png",
+        "images/top/taa.png",
+        "images/top/tyyuu.png",
+        "images/top/jyu.png",
+        "images/top/tt.png",
+        "images/top/ty.png",
+        "images/top/tr.png",
+        "images/top/tq.png"
+    ],
+    "t_links":[
+        "https://www.ta.com",
+        "https://www.tq.com",
+        "https://www.ty.com",
+        "https://www.yu.com",
+        "https://www.ll.com",
+        "https://www.tyo.com",
+        "https://www.t2.com"
+    ],
+    "tweets_selected":[2,5],   //  second image in tweets,   5th link in  t_links
+    "videos": [
+        "http://www.youtube.com/embed/v1",
+        "http://www.youtube.com/embed/v3",
+        "http://www.youtube.com/embed/v6",
+        "http://www.youtube.com/embed/v8",
+        "http://www.youtube.com/embed/v9",
+        "http://www.youtube.com/embed/v10",
+        "http://www.youtube.com/embed/v12",
+        "http://www.youtube.com/embed/v11",
+        "http://www.youtube.com/embed/v100",
+        "http://www.youtube.com/embed/v23",
+        "http://www.youtube.com/embed/v34"
+    ],
+    "video_selected" : 5  //  select 5th image
+  }
